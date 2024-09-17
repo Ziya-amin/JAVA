c7632fb706e257c5cf3034a3a49cc861cd5aeb9e
Ziya-amin <ziyaamin2022@gmail.com>
2024-09-17 11:40:07 +0530

    Second commit

--- DAY_02/DataType.java
+++ DAY_02/DataType.java
@@ -2,98 +2,58 @@ package DAY_02;
 
 public class DataType {
     public static void main(String[] args) {
-        // Primivite Datatype
-        // int a= 10;                             //
-        // boolean b= true;                      //stores only true and false value
-        // short c= 4;
-        // long d= 83748;
-        // float e= 8.74f;
-        // double f= 73.156846515321;
-        // char g='g';
-        // byte h= 76;
-        // System.out.println(a);
+
+        // Primitive DataType
+        int a= 10;                            //4 bytes, range= -2^31 to (2^31)-1
+        boolean b= true;                      //1 bit,   stores only true and false value
+        short c= 4;                           //2 bytes, range= -2^15 to (2^15)-1
+        long d= 83748;                        //8 bytes, range= -2^63 to (2^63)-1
+        float e= 8.74f;                       //4 bytes, shows only six decimal place with round off
+        double f= 73.156846515321;            //8 bytes, shows only 15 decimal places
+        char g='g';                           //2 bytes, compiler convert char into their ascii value while performing any operation and treat it as integers
+        byte h= 76;                           //1 byte,  range= -2^7 to (2^7)-1
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
 
 
 
-        // Range of Datatype
+        // some operation
 
-        // int miniInt= Integer.MIN_VALUE;
-        // int maxInt= Integer.MAX_VALUE;
-        // System.out.println(miniInt);
-        // System.out.println(maxInt);
+        int miniInt= Integer.MIN_VALUE;
+        int maxInt= Integer.MAX_VALUE;
+        System.out.println(miniInt);
+        System.out.println(maxInt);
 
-        // short miniSh= Short.MIN_VALUE;
-        // short maxSh= Short.MAX_VALUE;
-        // System.out.println(miniSh);
-        // System.out.println(maxSh);
+        short miniSh= Short.MIN_VALUE;
+        short maxSh= Short.MAX_VALUE;
+        System.out.println(miniSh);
+        System.out.println(maxSh);
 
-        // long miniLg= Long.MIN_VALUE;
-        // long maxLg= Long.MAX_VALUE;
-        // System.out.println(miniLg);
-        // System.out.println(maxLg);
+        long miniLg= Long.MIN_VALUE;
+        long maxLg= Long.MAX_VALUE;
+        System.out.println(miniLg);
+        System.out.println(maxLg);
 
-        // byte miniBt= Byte.MIN_VALUE;
-        // byte maxBt= Byte.MAX_VALUE;
-        // System.out.println(miniBt);
-        // System.out.println(maxBt);
+        byte miniBt= Byte.MIN_VALUE;
+        byte maxBt= Byte.MAX_VALUE;
+        System.out.println(miniBt);
+        System.out.println(maxBt);
 
-        // float miniFl= Float.MIN_VALUE;
-        // float maxFl= Float.MAX_VALUE;
-        // System.out.println(miniFl);
-        // System.out.println(maxFl);
-
-        // double miniDbl= Double.MIN_VALUE;
-        // double maxDbl= Double.MAX_VALUE;
-        // System.out.println(miniDbl);
-        // System.out.println(maxDbl);
+        char o= 'o';
+        char p= 'p';
+        System.out.println(o+1);
+        System.out.println(p+2);
+        System.out.println(p-o);
     }
 
 }
 
-    // public static void main(String[] args) {
-    //     for (int i=0; i<=10; i++){
-    //         System.out.println(i);
-    //     }
-    // }
-
-
-
-
-
-// public static void main(String[] args) {
-//     for (int i=0; i<=10; i++){
-//         if (i%2==0){
-//             System.out.println(i)
-//         }
-//     }
-// }
-
-
-
-
-
-// public static void main(String[] args) {
-//     int sum=0;
-//     int i=1;
-//     for (i<=10; i++){
-//         sum=sum+i;
-//     }
-//     System.out.println(sum);
-// }
-
-
-
-
-// public static void main(String[] args){
-//     for (int i=10; i>=0; i--){
-//         System.out.println(i)
-//     }
-//     // int i=10;
-//     while(i>=0){
-//         System.out.println(i);
-//         i--;
-//     }
-// }
 
 
 
