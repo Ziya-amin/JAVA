a14829af23a9f19116ea4caba49fe15c3bb5c048
Ziya-amin <ziyaamin2022@gmail.com>
2024-09-17 11:11:01 +0530

    First commit

---
+++ DAY_01/HelloWorld.java
@@ -0,0 +1,7 @@
+package DAY_01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" Hello World :) ");
+    }
+}
---
+++ DAY_02/DataType.java
@@ -0,0 +1,99 @@
+package DAY_02;
+
+public class DataType {
+    public static void main(String[] args) {
+        // Primivite Datatype
+        // int a= 10;                             //
+        // boolean b= true;                      //stores only true and false value
+        // short c= 4;
+        // long d= 83748;
+        // float e= 8.74f;
+        // double f= 73.156846515321;
+        // char g='g';
+        // byte h= 76;
+        // System.out.println(a);
+
+
+
+        // Range of Datatype
+
+        // int miniInt= Integer.MIN_VALUE;
+        // int maxInt= Integer.MAX_VALUE;
+        // System.out.println(miniInt);
+        // System.out.println(maxInt);
+
+        // short miniSh= Short.MIN_VALUE;
+        // short maxSh= Short.MAX_VALUE;
+        // System.out.println(miniSh);
+        // System.out.println(maxSh);
+
+        // long miniLg= Long.MIN_VALUE;
+        // long maxLg= Long.MAX_VALUE;
+        // System.out.println(miniLg);
+        // System.out.println(maxLg);
+
+        // byte miniBt= Byte.MIN_VALUE;
+        // byte maxBt= Byte.MAX_VALUE;
+        // System.out.println(miniBt);
+        // System.out.println(maxBt);
+
+        // float miniFl= Float.MIN_VALUE;
+        // float maxFl= Float.MAX_VALUE;
+        // System.out.println(miniFl);
+        // System.out.println(maxFl);
+
+        // double miniDbl= Double.MIN_VALUE;
+        // double maxDbl= Double.MAX_VALUE;
+        // System.out.println(miniDbl);
+        // System.out.println(maxDbl);
+    }
+
+}
+
+    // public static void main(String[] args) {
+    //     for (int i=0; i<=10; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+
+
+
+
+
+// public static void main(String[] args) {
+//     for (int i=0; i<=10; i++){
+//         if (i%2==0){
+//             System.out.println(i)
+//         }
+//     }
+// }
+
+
+
+
+
+// public static void main(String[] args) {
+//     int sum=0;
+//     int i=1;
+//     for (i<=10; i++){
+//         sum=sum+i;
+//     }
+//     System.out.println(sum);
+// }
+
+
+
+
+// public static void main(String[] args){
+//     for (int i=10; i>=0; i--){
+//         System.out.println(i)
+//     }
+//     // int i=10;
+//     while(i>=0){
+//         System.out.println(i);
+//         i--;
+//     }
+// }
+
+
+
