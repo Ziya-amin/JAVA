2afd3b2fd16a4d923edfb4cc677af753fb7f38af
Ziya-amin <ziyaamin2022@gmail.com>
2024-09-20 00:37:15 +0530

    third commit

---
+++ DAY_03/Operators.java
@@ -0,0 +1,81 @@
+public class Operators{
+    public static void main(String[] args) {
+
+        //Arithmetic Operators = Arithmetic operators are used to perform common mathematical operations.
+        // int a = 5;
+        // int b = 8;
+        // System.out.println(a+b);                              // Addition
+        // System.out.println(a-b);                              // Subtraction
+        // System.out.println(a*b);                              // Mutiplication
+        // System.out.println(a/b);                              // Division
+        // System.out.println(a%b);                              // Modulus gives remainder
+        // System.out.println(++a);                              // Increment then print
+        // System.out.println(--b);                              // Decrement then print
+        // System.out.println(b--);                              // print then Decrement cz of colon
+        // System.out.println(b++ + a);                          // '+' performing operation
+        // System.out.println(b++ + " " + a++ + a);              // '+' performing concatenation
+
+
+        //Assignment Operators
+        int x;
+        System.out.println(x = 5);
+        System.out.println(x += 3);
+        System.out.println(x -= 3);
+        System.out.println(x *= 3);
+        System.out.println(x /= 3);
+        System.out.println(x %= 3);
+        System.out.println(x &= 3);
+        System.out.println(x |= 3);
+        System.out.println(x ^= 3);
+        System.out.println(x >>= 3);
+        System.out.println(x <<= 3);
+    }        
+}
+
+
+    
+    
+    // public static void main(String[] args) {
+    //     for (int i=0; i<=10; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+
+
+
+
+
+// public static void main(String[] args) {
+//     for (int i=0; i<=10; i++){
+//         if (i%2==0){
+//             System.out.println(i)
+//         }
+//     }
+// }
+
+
+
+
+
+// public static void main(String[] args) {
+//     int sum=0;
+//     int i=1;
+//     for (i<=10; i++){
+//         sum=sum+i;
+//     }
+//     System.out.println(sum);
+// }
+
+
+
+
+// public static void main(String[] args){
+//     for (int i=10; i>=0; i--){
+//         System.out.println(i)
+//     }
+//     // int i=10;
+//     while(i>=0){
+//         System.out.println(i);
+//         i--;
+//     }
+// }
