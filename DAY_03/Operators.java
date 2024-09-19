c1c4e107f3228dec164e77a2095c410de1e3c236
Ziya-amin <ziyaamin2022@gmail.com>
2024-09-20 01:08:15 +0530

    Third Commit

--- DAY_03/Operators.java
+++ DAY_03/Operators.java
@@ -18,22 +18,40 @@ public class Operators{
 
         //Assignment Operators
         int x;
-        System.out.println(x = 5);
-        System.out.println(x += 3);
-        System.out.println(x -= 3);
-        System.out.println(x *= 3);
-        System.out.println(x /= 3);
-        System.out.println(x %= 3);
-        System.out.println(x &= 3);
-        System.out.println(x |= 3);
-        System.out.println(x ^= 3);
-        System.out.println(x >>= 3);
-        System.out.println(x <<= 3);
-    }        
-}
+        System.out.println(x = 5);                               
+        // System.out.println(x += 3);
+        // System.out.println(x -= 3);
+        // System.out.println(x *= 3);
+        // System.out.println(x /= 3);
+        // System.out.println(x %= 3);
+        // System.out.println(x &= 3);
+        // System.out.println(x |= 3);
+        // System.out.println(x ^= 3);
+        // System.out.println(x >>= 3);
+        // System.out.println(x <<= 3);
 
 
-    
+        //Comparison Operators
+        int y = 6;
+        // System.out.println(x == y);                               // Equal to
+        // System.out.println(x != y);                               // Not equal
+        // System.out.println(x > y);                                // Greater than
+        // System.out.println(x < y);                                // Less than
+        // System.out.println(x >= y);                               // Greater than or equal to
+        // System.out.println(x <= y);                               // Less than or equal to
+
+
+        //Logical Operators
+        System.out.println(x < 5 &&  x < 10);                        // Logical "and (&&)" Returns true if both statements are true
+        System.out.println(x < 5 || x < 4	);                       // Logical "or (||)" Returns true if one of the statements is true
+        System.out.println(!(x < 5 && x < 10));                      // Logical "not (!)" Reverse the result, returns false if the result is true
+    }
+}    
+
+
+
+
+
     
     // public static void main(String[] args) {
     //     for (int i=0; i<=10; i++){
